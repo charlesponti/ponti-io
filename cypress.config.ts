d9c6aec046b113a5476d7919c687c2986e3381c4
charlesponti <cj@ponti.io>
2022-10-11 21:56:10 -0700

    fix(cyrpess) format cypress files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      return require('./cypress/plugins/index.js')(on, config);
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
   },
 
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
 });
