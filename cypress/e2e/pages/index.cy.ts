d1ad310ea7cf98911d688ba4789fe89f3c069ff6
charlesponti <cj@ponti.io>
2023-09-24 10:19:32 -0700

    test(app/index): fix landing page e2e test

--- cypress/e2e/pages/index.cy.ts
+++ cypress/e2e/pages/index.cy.ts
@@ -1,14 +1,9 @@
-import { beforeEach, cy, describe, it } from 'local-cypress';
-
 describe('Index Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('should display correct heading', () => {
-    cy.get('h1').should(
-      'contain',
-      'Next.js + Tailwind CSS + TypeScript Starter'
-    );
+    cy.get('h1').should('contain', 'Ponti Studios');
   });
 });
