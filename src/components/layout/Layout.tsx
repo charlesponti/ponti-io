1778c4e5dd4c2e215aa28f330047752c86b73401
charlesponti <cj@ponti.io>
2023-10-31 10:46:52 +0000

    feat(app): switch to black bg

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div data-theme='luxury'>
-      <div className='text-white antialiased dark:bg-gray-800 dark:text-white'>
+      <div className='text-white antialiased dark:bg-black dark:text-white'>
         {children}
       </div>
     </div>
