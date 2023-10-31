8a4b3601837a5ce3729eeeb41783547f2d5d2b0d
charlesponti <cj@ponti.io>
2023-10-31 15:02:56 +0000

    feat(app/home): remove footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-import Link from 'next/link';
 import * as React from 'react';
 
 import Layout from 'src/components/layout/Layout';
 import Seo from 'src/components/Seo';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
 
 export default function HomePage() {
   return (
@@ -25,19 +16,6 @@ export default function HomePage() {
             <p className='mt-2 text-md mb-3'>
               The next-generation production company.
             </p>
-
-            <footer className='absolute bottom-2'>
-              <span className='text-accent-content'>
-                © {new Date().getFullYear()}
-              </span>{' '}
-              by{' '}
-              <Link
-                className='text-neutral-content'
-                href='https://twitter.com/thechaseponti'
-              >
-                @thechaseponti
-              </Link>
-            </footer>
           </div>
         </section>
       </main>
