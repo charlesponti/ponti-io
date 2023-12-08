a13ff9937188f6a67d0b50ad020e365e3b9758da
Charles Ponti <cj@ponti.io>
2023-12-07 16:27:05 -0800

    feat: update Hero copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,12 @@ import Services from 'src/components/landing/Services';
 import Layout from 'src/components/layout/Layout';
 import Seo from 'src/components/Seo';
 
+const Section = ({ children }: { children: React.ReactNode }) => (
+  <div className='bg-white rounded-lg shadow-lg overflow-hidden my-9 p-8'>
+    {children}
+  </div>
+);
+
 export default function HomePage() {
   return (
     <Layout>
@@ -19,34 +25,31 @@ export default function HomePage() {
       <main>
         <div className='layout flex min-h-screen flex-col md:w-[700px]'>
           {/* Hero */}
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden mt-9 mb-9'>
-            <div className='p-8 pb-11'>
-              <h1 className='mb-6 text-3xl text-indigo-500'>
+          <Section>
+            {/* <h1 className='mb-6 text-3xl text-indigo-500'>
                 The next-generation production company.
-              </h1>
-              <p className='text-lg md:text-xl text-primary-content'>
-                We build products people love.{' '}
-                <span className='font-semibold'>Period.</span>
-              </p>
-              <p className='mt-4 text-primary-content'>
-                We're a team of international engineers, designers, and data
-                scientists <b>obsessed</b> with moving the ball forward.
-              </p>
-              <p className='mt-4 text-primary-content'>
-                We use{' '}
-                <b>
-                  <i>
-                    design thinking, machine learning, and exploratory data
-                    analysis
-                  </i>
-                </b>{' '}
-                to meticulously build{' '}
-                <span className='font-semibold'>customer-focused</span>{' '}
-                solutions that enable <b>humans</b> to achieve exponential
-                growth.
-              </p>
-            </div>
-          </div>
+              </h1> */}
+            <p className='text-lg md:text-4xl text-primary-content font-semibold mt-4'>
+              We don't just build products.{' '}
+            </p>
+            <p className='text-lg md:text-3xl mt-2 text-primary-content font-semibold'>
+              We build solutions people{'   '}
+              <span className='font-extrabold'>love.</span>
+            </p>
+            <p className='text-primary-content mt-8'>
+              We're a team of international engineers, designers, and data
+              scientists who use{' '}
+              <b>
+                <i>
+                  design thinking, machine learning, and exploratory data
+                  analysis
+                </i>
+              </b>{' '}
+              to meticulously build{' '}
+              <span className='font-semibold'>customer-focused</span> solutions
+              that enable <b>humans</b> to achieve exponential growth.
+            </p>
+          </Section>
           <Services />
         </div>
       </main>
