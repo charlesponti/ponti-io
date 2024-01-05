3c040a3c5188a65e3dc0069ce44b1f4cb6fd39d9
Charles Ponti <cj@ponti.io>
2024-01-05 13:37:55 -0800

    fix: use renderWithProviders in tests

--- components/Cards/Cards.spec.tsx
+++ components/Cards/Cards.spec.tsx
@@ -1,16 +1,16 @@
-import { render } from '@testing-library/react';
+import renderWithProviders from '@/tests/testUtils';
 import { describe, expect, test } from 'vitest';
 
 import Cards from '.';
 
 describe('Cards', () => {
   test('should render global states', async () => {
-    const { container } = render(<Cards countryCode='global' />);
+    const { container } = renderWithProviders(<Cards countryCode='global' />);
     expect(container).toMatchSnapshot();
   });
 
   test('should render US data', () => {
-    const { container } = render(<Cards countryCode='US' />);
+    const { container } = renderWithProviders(<Cards countryCode='US' />);
     expect(container).toMatchSnapshot();
   });
 });
