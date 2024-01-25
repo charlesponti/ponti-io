0b7225c5e3e763b0c434eec33d94a6a5e3b2d8de
Charles Ponti <cj@ponti.io>
2024-01-24 20:50:34 -0800

    feat(landing): update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -51,60 +51,53 @@ const _Section = ({
 
 export default function Home() {
   return (
-    <main className='flex flex-col justify-between items-center'>
-      <div className='container flex flex-col'>
-        {/* Hero */}
-        <div className='flex flex-col justify-center h-[350px] p-4 mb-16'>
-          <div
-            className='bg-primary overflow-hidden max-w-3xl'
-            style={{ textWrap: 'pretty' }}
-          >
-            <p className='text-4xl md:text-6xl mt-4 text-primary lg:text-5xl lg:leading-[3.5rem]'>
-              We build <b className='text-slate-400'>lovable</b> technology for{' '}
-              <b className='text-slate-400'>humans</b>.
+    <div className='container flex flex-col'>
+      {/* Hero */}
+      <div className='flex flex-col justify-center h-[350px] p-4 mb-16'>
+        <div
+          className='bg-primary overflow-hidden max-w-3xl'
+          style={{ textWrap: 'pretty' }}
+        >
+          <p className='text-4xl md:text-6xl mt-4 text-primary lg:text-5xl lg:leading-[3.5rem]'>
+            We build <b className='text-slate-400'>lovable</b> technology for{' '}
+            <b className='text-slate-400'>humans</b>.
+          </p>
+          <p className='text-primary mt-6 text-lg md:text-xl'>
+            We're a diverse team of international designers and technologists
+            using <i>humanity-centered design</i> and <i>data science</i> to
+            produce efficient and effective solutions that solve <i>sticky</i>{' '}
+            problems.
+          </p>
+        </div>
+      </div>
+      <div className='flex flex-col justify-center p-4 bg-black text-white mb-24 pt-8 pb-12'>
+        <h2 className='text-6xl md:text-8xl mb-8'>Services</h2>
+        <div className='flex flex-col gap-4 lg:flex-row lg:space-between'>
+          <div className='flex flex-col justify-center flex-1 font-light'>
+            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3'>
+              Product Feasbility & Workshops
             </p>
-            <p className='text-primary mt-6 text-lg md:text-xl'>
-              We're a diverse team of international designers and technologists
-              using <i>humanity-centered design</i> and <i>data science</i> to
-              produce efficient and effective solutions that solve <i>sticky</i>{' '}
-              problems.
+            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3'>
+              Design & Engineering
+            </p>
+            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3'>
+              Branding & Marketing
+            </p>
+            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3'>
+              Events & Experienes
+            </p>
+            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3'>
+              Social Media & Community
             </p>
           </div>
-        </div>
-        <div className='flex flex-col justify-center p-4 bg-black text-white mb-24 pt-8 pb-12'>
-          <h2 className='text-6xl md:text-8xl mb-8'>Services</h2>
-          <div className='flex flex-col gap-4 lg:flex-row lg:space-between'>
-            <div className='flex flex-col justify-center flex-1 font-light'>
-              <p className='text-4xl lg:text-4xl text-slate-600 mb-1 md:mb-3'>
-                Ideation & MVPs
-              </p>
-              <p className='text-4xl lg:text-4xl text-slate-600 mb-1 md:mb-3'>
-                Design & Engineering
-              </p>
-              <p className='text-4xl lg:text-4xl text-slate-600 mb-1 md:mb-3'>
-                Branding
-              </p>
-              <p className='text-4xl lg:text-4xl text-slate-600 mb-1 md:mb-3'>
-                Products & Experiences
-              </p>
-              <p className='text-4xl lg:text-4xl text-slate-600 mb-1 md:mb-3'>
-                Social & Community
-              </p>
-            </div>
-            <div className='flex flex-col justify-center flex-1 text-2xl md:text-3xl lg:text-4xl font-light'>
-              We partner with clients across communications, brand design and
-              consulting, experience design, technology strategy and
-              engineering, media planning and buying, and relationship design.
-              {/* See how we can help */}
-            </div>
+          <div className='flex flex-col justify-center flex-1 text-2xl md:text-3xl lg:text-4xl font-light'>
+            We partner with clients across communications, brand design and
+            consulting, experience design, technology strategy and engineering,
+            media planning and buying, and relationship design.
+            {/* See how we can help */}
           </div>
         </div>
       </div>
-      {/* <div className='grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <Section {...sections[0]} />
-        <Section {...sections[1]} />
-        <Section {...sections[2]} />
-      </div> */}
-    </main>
+    </div>
   );
 }
