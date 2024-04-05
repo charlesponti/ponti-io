61e2b73f139b2af124569e4c438c68fc7b283f81
Charles Ponti <cj@ponti.io>
2024-04-04 18:57:54 -0700

    feat(repo): add biome

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,73 @@
-import { PropsWithChildren } from 'react';
-import { twMerge } from 'tailwind-merge';
+import type { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 const GradialCard = ({
-  children,
-  className,
+	children,
+	className,
 }: PropsWithChildren & {
-  className?: string;
+	className?: string;
 }) => (
-  <div
-    className={twMerge(
-      `flex flex-col justify-center w-part-full min-h-[75vh] px-4 mb-16 shadow-md bg-gradient-radial rounded-2xl`,
-      className,
-    )}
-    style={{ textWrap: 'balance' }}
-  >
-    {children}
-  </div>
+	<div
+		className={twMerge(
+			"flex flex-col justify-center w-part-full min-h-[75vh] px-4 mb-16 shadow-md bg-gradient-radial rounded-2xl",
+			className,
+		)}
+		style={{ textWrap: "balance" }}
+	>
+		{children}
+	</div>
 );
 
 export default function Home() {
-  return (
-    <div className='container flex flex-col items-center w-full max-w-[1200px] gap-6'>
-      <GradialCard className='from-slate-200 to-white'>
-        <p className='text-primary text-fluid-1 leading-fluid-2 md:text-center font-thin opacity-75 uppercase whitespace-pre-wrap'>
-          <span className='text-slate-800'>Lovable</span> technology for{' '}
-          <span className='text-slate-800'>humans</span>.
-        </p>
-      </GradialCard>
-      <GradialCard className='from-slate-200 to-white'>
-        <p className='text-primary text-lg md:text-4xl w-[90%] font-thin'>
-          We're a diverse team of international designers and technologists
-          using <i>humanity-centered design</i> and <i>data science</i> to
-          produce efficient and effective solutions that solve <i>sticky</i>{' '}
-          problems.
-        </p>
-      </GradialCard>
-      <GradialCard className='from-slate-200 to-white'>
-        <h2 className='mb-fluid-2 text-fluid-2'>Services</h2>
-        <div className='flex flex-col gap-12'>
-          <div className='flex flex-col justify-center flex-1 font-light gap-3'>
-            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light'>
-              Product Feasbility & Workshops
-            </p>
-            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light'>
-              Design & Engineering
-            </p>
-            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light'>
-              Branding & Marketing
-            </p>
-            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light'>
-              Events & Experienes
-            </p>
-            <p className='text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light'>
-              Social Media & Community
-            </p>
-          </div>
-        </div>
-      </GradialCard>
-      <GradialCard className='from-slate-200 to-white'>
-        <p className='text-primary text-lg md:text-4xl w-[90%] font-thin'>
-          We partner with clients across communications, brand design and
-          consulting, experience design, technology strategy and engineering,
-          media planning and buying, and relationship design.
-          {/* See how we can help */}
-        </p>
-      </GradialCard>
-    </div>
-  );
+	return (
+		<div className="container flex flex-col items-center w-full max-w-[1200px] gap-6">
+			<GradialCard className="bg-black">
+				<h1 className="text-6xl text-white font-thin">Hello, World!</h1>
+			</GradialCard>
+			<GradialCard className="from-slate-200 to-white">
+				<p className="text-primary text-fluid-1 leading-fluid-2 md:text-center font-thin opacity-75 uppercase whitespace-pre-wrap">
+					<span className="text-slate-800">Lovable</span> technology for{" "}
+					<span className="text-slate-800">humans</span>.
+				</p>
+			</GradialCard>
+			<GradialCard className="from-slate-200 to-white">
+				<p className="text-primary text-lg md:text-4xl w-[90%] font-thin">
+					We're a diverse team of international designers and technologists
+					using <i>humanity-centered design</i> and <i>data science</i> to
+					produce efficient and effective solutions that solve <i>sticky</i>{" "}
+					problems.
+				</p>
+			</GradialCard>
+			<GradialCard className="from-slate-200 to-white">
+				<h2 className="mb-fluid-2 text-fluid-2">Services</h2>
+				<div className="flex flex-col gap-12">
+					<div className="flex flex-col justify-center flex-1 font-light gap-3">
+						<p className="text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light">
+							Product Feasbility & Workshops
+						</p>
+						<p className="text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light">
+							Design & Engineering
+						</p>
+						<p className="text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light">
+							Branding & Marketing
+						</p>
+						<p className="text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light">
+							Events & Experienes
+						</p>
+						<p className="text-2xl md:text-4xl text-slate-600 mb-1 md:mb-3 font-light">
+							Social Media & Community
+						</p>
+					</div>
+				</div>
+			</GradialCard>
+			<GradialCard className="from-slate-200 to-white">
+				<p className="text-primary text-lg md:text-4xl w-[90%] font-thin">
+					We partner with clients across communications, brand design and
+					consulting, experience design, technology strategy and engineering,
+					media planning and buying, and relationship design.
+					{/* See how we can help */}
+				</p>
+			</GradialCard>
+		</div>
+	);
 }
--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,80 +1,77 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
-// import "@testing-library/jest-dom";
-import mediaQuery from 'css-mediaquery';
-import { HttpResponse, http } from 'msw';
-import { setupServer } from 'msw/node';
-import { afterAll, afterEach, beforeAll, beforeEach, vi } from 'vitest';
-import 'whatwg-fetch';
+import mediaQuery from "css-mediaquery";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll, beforeEach, vi } from "vitest";
+import "whatwg-fetch";
 
 // Allow router mocks.
-// eslint-disable-next-line no-undef
-vi.mock('next/navigation', () => require('next-router-mock'));
+vi.mock("next/navigation", () => require("next-router-mock"));
 
 import {
-  DESKTOP_RESOLUTION_HEIGHT,
-  DESKTOP_RESOLUTION_WIDTH,
-} from './testUtils';
+	DESKTOP_RESOLUTION_HEIGHT,
+	DESKTOP_RESOLUTION_WIDTH,
+} from "./testUtils";
 
 const COUNT = 1000;
 const base = {
-  data: {
-    confirmed: { value: COUNT },
-    deaths: { value: COUNT },
-    recovered: { value: COUNT },
-    lastUpdate: new Date().getTime(),
-  },
+	data: {
+		confirmed: { value: COUNT },
+		deaths: { value: COUNT },
+		recovered: { value: COUNT },
+		lastUpdate: new Date().getTime(),
+	},
 };
 
 const restHandlers = [
-  http.get('https://covid19.mathdro.id/api', () => {
-    return HttpResponse.json(base);
-  }),
-  http.get('https://covid19.mathdro.id/api/countries', () => {
-    return HttpResponse.json([
-      { name: 'US', iso2: 'US' },
-      { name: 'Canada', iso2: 'CA' },
-      { name: 'China', iso2: 'CN' },
-      { name: 'Italy', iso2: 'IT' },
-    ]);
-  }),
-  http.get('https://covid19.mathdro.id/api/countries/US', () => {
-    return HttpResponse.json({
-      confirmed: { value: COUNT },
-      deaths: { value: COUNT },
-      recovered: { value: COUNT },
-      lastUpdate: new Date().getTime(),
-    });
-  }),
-  http.get('https://covid19.mathdro.id/api/countries/global/confirmed', () => {
-    return HttpResponse.json([
-      {
-        confirmed: 1000,
-        deaths: 1000,
-        lastUpdate: 1234,
-        countryRegion: 'Test Country',
-        provinceState: 'Test State',
-        uid: 'test-uid',
-      },
-    ]);
-  }),
-  http.get('https://covid19.mathdro.id/api/countries/US/confirmed', () => {
-    return HttpResponse.json([
-      {
-        confirmed: 1000,
-        deaths: 1000,
-        lastUpdate: 1234,
-        county: 'Test County',
-        state: 'Test State',
-        uid: 'test-uid',
-      },
-    ]);
-  }),
+	http.get("https://covid19.mathdro.id/api", () => {
+		return HttpResponse.json(base);
+	}),
+	http.get("https://covid19.mathdro.id/api/countries", () => {
+		return HttpResponse.json([
+			{ name: "US", iso2: "US" },
+			{ name: "Canada", iso2: "CA" },
+			{ name: "China", iso2: "CN" },
+			{ name: "Italy", iso2: "IT" },
+		]);
+	}),
+	http.get("https://covid19.mathdro.id/api/countries/US", () => {
+		return HttpResponse.json({
+			confirmed: { value: COUNT },
+			deaths: { value: COUNT },
+			recovered: { value: COUNT },
+			lastUpdate: new Date().getTime(),
+		});
+	}),
+	http.get("https://covid19.mathdro.id/api/countries/global/confirmed", () => {
+		return HttpResponse.json([
+			{
+				confirmed: 1000,
+				deaths: 1000,
+				lastUpdate: 1234,
+				countryRegion: "Test Country",
+				provinceState: "Test State",
+				uid: "test-uid",
+			},
+		]);
+	}),
+	http.get("https://covid19.mathdro.id/api/countries/US/confirmed", () => {
+		return HttpResponse.json([
+			{
+				confirmed: 1000,
+				deaths: 1000,
+				lastUpdate: 1234,
+				county: "Test County",
+				state: "Test State",
+				uid: "test-uid",
+			},
+		]);
+	}),
 ];
 
 const server = setupServer(...restHandlers);
 
 // Start server before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
 //  Close server after all tests
 afterAll(() => server.close());
@@ -83,56 +80,55 @@ afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 beforeAll(() => {
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: (query: string) => {
-      function matchQuery(): boolean {
-        return mediaQuery.match(query, {
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
+	Object.defineProperty(window, "matchMedia", {
+		writable: true,
+		value: (query: string) => {
+			function matchQuery(): boolean {
+				return mediaQuery.match(query, {
+					width: window.innerWidth,
+					height: window.innerHeight,
+				});
+			}
 
-      const listeners: (() => void)[] = [];
-      const instance = {
-        matches: matchQuery(),
-        addEventListener: (_: 'change', listener: () => void): void => {
-          listeners.push(listener);
-        },
-        removeEventListener: (_: 'change', listener: () => void): void => {
-          const index = listeners.indexOf(listener);
-          if (index >= 0) {
-            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-            listeners.splice(index, 1);
-          }
-        },
-      };
-      window.addEventListener('resize', () => {
-        const change = matchQuery();
-        if (change !== instance.matches) {
-          instance.matches = change;
-          for (const listener of listeners) listener();
-        }
-      });
+			const listeners: (() => void)[] = [];
+			const instance = {
+				matches: matchQuery(),
+				addEventListener: (_: "change", listener: () => void): void => {
+					listeners.push(listener);
+				},
+				removeEventListener: (_: "change", listener: () => void): void => {
+					const index = listeners.indexOf(listener);
+					if (index >= 0) {
+						listeners.splice(index, 1);
+					}
+				},
+			};
+			window.addEventListener("resize", () => {
+				const change = matchQuery();
+				if (change !== instance.matches) {
+					instance.matches = change;
+					for (const listener of listeners) listener();
+				}
+			});
 
-      return instance;
-    },
-  });
-  Object.defineProperty(window, 'scrollTo', {
-    writable: true,
-    value: () => {},
-  });
-  Object.defineProperty(window, 'resizeTo', {
-    writable: true,
-    value: (width: number, height: number) => {
-      Object.assign(window, {
-        innerWidth: width,
-        innerHeight: height,
-      }).dispatchEvent(new window.Event('resize'));
-    },
-  });
+			return instance;
+		},
+	});
+	Object.defineProperty(window, "scrollTo", {
+		writable: true,
+		value: () => {},
+	});
+	Object.defineProperty(window, "resizeTo", {
+		writable: true,
+		value: (width: number, height: number) => {
+			Object.assign(window, {
+				innerWidth: width,
+				innerHeight: height,
+			}).dispatchEvent(new window.Event("resize"));
+		},
+	});
 });
 
 beforeEach(() => {
-  window.resizeTo(DESKTOP_RESOLUTION_WIDTH, DESKTOP_RESOLUTION_HEIGHT);
+	window.resizeTo(DESKTOP_RESOLUTION_WIDTH, DESKTOP_RESOLUTION_HEIGHT);
 });
