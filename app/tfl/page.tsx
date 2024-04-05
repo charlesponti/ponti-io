19731e0129f30d7b4fc8aa004b50f4202b0c253c
Charles Ponti <cj@ponti.io>
2024-04-04 18:57:54 -0700

    feat(repo): add biome

--- app/tfl/page.tsx
+++ app/tfl/page.tsx
@@ -1,107 +1,113 @@
-'use client';
+"use client";
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 import {
-  APIProvider,
-  InfoWindow,
-  Map,
-  Marker,
-  useMap,
-  useMapsLibrary,
-} from '@vis.gl/react-google-maps';
-import React, { useEffect } from 'react';
-import { Camera, CamerasResponse } from '../../utils/types';
+	APIProvider,
+	InfoWindow,
+	Marker,
+	Map as ReactGoogleMaps,
+	useMap,
+	useMapsLibrary,
+} from "@vis.gl/react-google-maps";
+import React, { useEffect } from "react";
+import type { Camera, CamerasResponse } from "../../utils/types";
 
 const TFLMap = () => {
-  const map = useMap();
-  const google = useMapsLibrary('core');
-  const [infowindow, setInfowindow] = React.useState<{
-    content: string;
-    id: string;
-    file: string;
-    lat: number;
-    lng: number;
-  } | null>(null);
-  const { data, isLoading } = useQuery<CamerasResponse>({
-    queryKey: ['cameras'],
-    queryFn: async () => {
-      const res = await fetch('/tfl/api/');
-      const json = await res.json();
-      return json.cameras;
-    },
-  });
+	const map = useMap();
+	const google = useMapsLibrary("core");
+	const [infowindow, setInfowindow] = React.useState<{
+		content: string;
+		id: string;
+		file: string;
+		lat: number;
+		lng: number;
+	} | null>(null);
+	const { data, isLoading } = useQuery<CamerasResponse>({
+		queryKey: ["cameras"],
+		queryFn: async () => {
+			const res = await fetch("/tfl/api/");
+			const json = await res.json();
+			return json.cameras;
+		},
+	});
 
-  useEffect(() => {
-    // Once the map is loaded, add the cameras to it
-    if (data && map && google) {
-      map.setCenter(new google.LatLng(51.508742, -0.12085));
-      map.setZoom(11);
-    }
-  }, [data, google, map]);
+	useEffect(() => {
+		// Once the map is loaded, add the cameras to it
+		if (data && map && google) {
+			map.setCenter(new google.LatLng(51.508742, -0.12085));
+			map.setZoom(11);
+		}
+	}, [data, google, map]);
 
-  const onMarkerClick = (camera: Camera) => {
-    setInfowindow({
-      content: camera.location,
-      file: camera.file,
-      id: camera.id,
-      lat: +camera.lat,
-      lng: +camera.lng,
-    });
-  };
+	const onMarkerClick = (camera: Camera) => {
+		setInfowindow({
+			content: camera.location,
+			file: camera.file,
+			id: camera.id,
+			lat: +camera.lat,
+			lng: +camera.lng,
+		});
+	};
 
-  if (isLoading) {
-    return (
-      <div className='h-96 p-4'>
-        <div>Loading...</div>
-      </div>
-    );
-  }
+	if (isLoading) {
+		return (
+			<div className="h-96 p-4">
+				<div>Loading...</div>
+			</div>
+		);
+	}
 
-  return (
-    <div className='h-96 p-4'>
-      <Map
-        className='h-5/6'
-        zoom={3}
-        center={{ lat: 22.54992, lng: 0 }}
-        gestureHandling='greedy'
-        disableDefaultUI={true}
-      >
-        {infowindow ? (
-          <InfoWindow position={{ lat: infowindow.lat, lng: infowindow.lng }}>
-            <div className='flex flex-col py-2'>
-              <img
-                alt={infowindow.content}
-                className='camera-pic'
-                style={{ marginBottom: '10px', width: '100%' }}
-                src={`http://www.tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/${infowindow.file}`}
-              />
-              <p className='text-lg font-semibold'>{infowindow.content}</p>
-            </div>
-          </InfoWindow>
-        ) : null}
-        {data?.map((camera) => (
-          <Marker
-            data-camera={camera.id}
-            key={camera.id}
-            position={{ lat: +camera.lat, lng: +camera.lng }}
-            title={camera.location}
-            icon={{
-              anchor: google && new google.Point(30, 30),
-              scaledSize: google && new google.Size(25, 25),
-              url: '/camera.png',
-            }}
-            onClick={() => onMarkerClick(camera)}
-          />
-        ))}
-      </Map>
-    </div>
-  );
+	return (
+		<div className="h-96 p-4">
+			<ReactGoogleMaps
+				className="h-5/6"
+				zoom={3}
+				center={{ lat: 22.54992, lng: 0 }}
+				gestureHandling="greedy"
+				disableDefaultUI={true}
+			>
+				{infowindow ? (
+					<InfoWindow position={{ lat: infowindow.lat, lng: infowindow.lng }}>
+						<div className="flex flex-col py-2">
+							<img
+								alt={infowindow.content}
+								className="camera-pic"
+								style={{ marginBottom: "10px", width: "100%" }}
+								src={`http://www.tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/${infowindow.file}`}
+							/>
+							<p className="text-lg font-semibold">{infowindow.content}</p>
+						</div>
+					</InfoWindow>
+				) : null}
+				{data?.map((camera) => (
+					<Marker
+						data-camera={camera.id}
+						key={camera.id}
+						position={{ lat: +camera.lat, lng: +camera.lng }}
+						title={camera.location}
+						icon={{
+							anchor: google && new google.Point(30, 30),
+							scaledSize: google && new google.Size(25, 25),
+							url: "/camera.png",
+						}}
+						onClick={() => onMarkerClick(camera)}
+					/>
+				))}
+			</ReactGoogleMaps>
+		</div>
+	);
 };
 
 export default function TFLCameras() {
-  return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <TFLMap />
-    </APIProvider>
-  );
+	const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+	if (!apiKey) {
+		throw new Error("Missing Google Maps API key");
+	}
+
+	return (
+		<APIProvider apiKey={apiKey}>
+			<TFLMap />
+		</APIProvider>
+	);
 }
--- components/Cards/index.tsx
+++ components/Cards/index.tsx
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import Card from '@/components/Card';
-import CountUpTo from '@/components/CountUpTo';
-import Grid from '@/components/Grid';
-import { fetchCountryData, fetchData } from '@/utils/corona.api';
-import { useQuery } from '@tanstack/react-query';
-import styles from './Cards.module.css';
+import Card from "@/components/Card";
+import CountUpTo from "@/components/CountUpTo";
+import Grid from "@/components/Grid";
+import { fetchCountryData, fetchData } from "@/utils/corona.api";
+import { useQuery } from "@tanstack/react-query";
+import styles from "./Cards.module.css";
 
 const Title = styled.h3`
   margin-bottom: 8px;
@@ -23,64 +23,64 @@ const DateText = styled.span`
 `;
 
 type CardsProps = {
-  readonly countryCode: string;
+	readonly countryCode: string;
 };
 function Cards({ countryCode }: CardsProps): JSX.Element | null {
-  const { data } = useQuery({
-    queryKey: ['fetchData', countryCode],
-    queryFn: () => {
-      if (countryCode === 'global') {
-        return fetchData();
-      } else {
-        return fetchCountryData(countryCode);
-      }
-    },
-    enabled: countryCode !== undefined,
-  });
+	const { data } = useQuery({
+		queryKey: ["fetchData", countryCode],
+		queryFn: () => {
+			if (countryCode === "global") {
+				return fetchData();
+			}
 
-  if (!data?.confirmed) {
-    return null;
-  }
+			return fetchCountryData(countryCode);
+		},
+		enabled: countryCode !== undefined,
+	});
 
-  return (
-    <Grid className={styles.container}>
-      <Grid xs={11} className='card'>
-        <Card>
-          <Grid
-            className={`${styles['cards-container']} justify-between`}
-            data-testid='cards-container'
-          >
-            <Grid
-              xs={12}
-              sm={6}
-              className={styles.stat}
-              data-testid='cases-card'
-            >
-              <p className={styles.cardStat}>
-                <CountUpTo value={data.confirmed} />
-              </p>
-              <Title>Cases</Title>
-            </Grid>
-            <Grid
-              xs={12}
-              sm={6}
-              className={styles.stat}
-              data-testid='deaths-card'
-            >
-              <p className={styles.cardStat}>
-                <CountUpTo value={data.deaths} />
-              </p>
-              <Title>Deaths</Title>
-            </Grid>
-            <LastUpdated data-testid='cards-summary'>
-              as of
-              <DateText>{new Date(data.lastUpdate).toDateString()}</DateText>
-            </LastUpdated>
-          </Grid>
-        </Card>
-      </Grid>
-    </Grid>
-  );
+	if (!data?.confirmed) {
+		return null;
+	}
+
+	return (
+		<Grid className={styles.container}>
+			<Grid xs={11} className="card">
+				<Card>
+					<Grid
+						className={`${styles["cards-container"]} justify-between`}
+						data-testid="cards-container"
+					>
+						<Grid
+							xs={12}
+							sm={6}
+							className={styles.stat}
+							data-testid="cases-card"
+						>
+							<p className={styles.cardStat}>
+								<CountUpTo value={data.confirmed} />
+							</p>
+							<Title>Cases</Title>
+						</Grid>
+						<Grid
+							xs={12}
+							sm={6}
+							className={styles.stat}
+							data-testid="deaths-card"
+						>
+							<p className={styles.cardStat}>
+								<CountUpTo value={data.deaths} />
+							</p>
+							<Title>Deaths</Title>
+						</Grid>
+						<LastUpdated data-testid="cards-summary">
+							as of
+							<DateText>{new Date(data.lastUpdate).toDateString()}</DateText>
+						</LastUpdated>
+					</Grid>
+				</Card>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default Cards;
