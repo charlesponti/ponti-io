30853c8cca5e03ed143979f2adddb07824505685
Charles Ponti <cj@ponti.io>
2024-04-13 12:38:26 -0700

    fix(test): remove .only and rename test action name

--- tests/page.spec.tsx
+++ tests/page.spec.tsx
@@ -6,7 +6,7 @@ import App from "../app/corona/page";
 import renderWithProviders from "./test.utils";
 
 describe("App", () => {
-	test.only("renders CountryPicker after loading successfully", async () => {
+	test("renders CountryPicker after loading successfully", async () => {
 		renderWithProviders(<App />);
 		expect(await screen.findByText("US")).toBeInTheDocument();
 	});
