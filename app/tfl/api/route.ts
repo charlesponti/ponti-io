4a2919ebdfe5fe5a16074f18153a5c2d166a70be
Charles Ponti <cj@ponti.io>
2024-04-14 19:21:25 -0700

    feat(app/tfl): format cameras on worker load

--- app/tfl/api/route.ts
+++ app/tfl/api/route.ts
@@ -8,28 +8,28 @@ type Properties = {
 	[PropertyKey.imageUrl]: string;
 };
 
+const formattedCameras: Cameras = (cameras as BaseCamera[]).map((camera) => {
+	const properties = camera.additionalProperties.reduce(
+		(acc: { [key: string]: string }, property) => {
+			acc[property.key] = property.value;
+			return acc;
+		},
+		{},
+	) as Properties;
+
+	return {
+		commonName: camera.commonName,
+		lat: camera.lat,
+		lng: camera.lon,
+		id: camera.id,
+		available: properties.available,
+		videoUrl: properties.videoUrl,
+		view: properties.view,
+		imageUrl: properties.imageUrl,
+	};
+});
+
 export async function GET(): Promise<Response> {
-	const formattedCameras: Cameras = (cameras as BaseCamera[]).map((camera) => {
-		const properties = camera.additionalProperties.reduce(
-			(acc: { [key: string]: string }, property) => {
-				acc[property.key] = property.value;
-				return acc;
-			},
-			{},
-		) as Properties;
-
-		return {
-			commonName: camera.commonName,
-			lat: camera.lat,
-			lng: camera.lon,
-			id: camera.id,
-			available: properties.available,
-			videoUrl: properties.videoUrl,
-			view: properties.view,
-			imageUrl: properties.imageUrl,
-		};
-	});
-
 	return Response.json({
 		cameras: formattedCameras,
 	});
