b4794497a3e373f5b6c8441112eaf95a78836eee
Charles Ponti <cj@ponti.io>
2024-07-04 12:23:06 -0700

    feat: update text and border styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 export default function Home() {
 	return (
-		<div className="container flex flex-col w-full max-w-[1200px] mb-24 lg:border-x-2 px-2 md:px-8">
-			<section className="flex h-[calc(100vh-95px)] mt-[95px]">
-				<h2 className="flex flex-col justify-center text-balance font-serif">
-					<p className="font-serif font-medium text-black text-7xl tracking-tighter italic mb-4">
-						Ponti Studios
-					</p>
-					<p className="text-3xl text-lime-600 tracking-tighter font-light">
-						We build lovable technology for humans.
-					</p>
-				</h2>
-			</section>
-		</div>
+		<section className="flex h-[calc(100vh-95px)] mt-[95px] w-full">
+			<h2 className="flex flex-col justify-center text-balance font-serif">
+				<p className="font-serif font-medium text-black text-5xl lg:text-7xl tracking-tighter italic mb-4">
+					Products don't have to suck.
+				</p>
+				<p className="text-2xl lg:text-3xl text-lime-600 tracking-tighter font-light">
+					We build lovable technology for humans.
+				</p>
+			</h2>
+		</section>
 	);
 }
