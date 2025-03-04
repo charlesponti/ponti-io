794284470c0cf6632b3448fa03f58f823e4e6d73
Charles Ponti <cj@ponti.io>
2025-03-03 17:41:54 -0800

    feat: move Price component to services; remove redundant file

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,3 @@
-import Price from "@/components/landing/price";
-
 const FEATURES = {
 	showPrice: false,
 };
@@ -110,3 +108,22 @@ export default function Services() {
 		</div>
 	);
 }
+
+const Price = ({ price, pricePer }: { price?: number; pricePer?: string }) => {
+	return (
+		<>
+			{price ? (
+				<>
+					${price}
+					{pricePer ? (
+						<span data-testid="price-per" className="text-gray-600 text-base">
+							/{pricePer}
+						</span>
+					) : null}
+				</>
+			) : (
+				<span>Price upon consultation</span>
+			)}
+		</>
+	);
+};
