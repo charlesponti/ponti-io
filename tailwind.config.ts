14794d1002cc9f85634f4640074fea2ecdc257ae
Charles Ponti <cj@ponti.io>
2025-03-24 10:33:37 -0700

    chore: add shadcn components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	plugins: [require("@tailwindcss/forms"), require("daisyui")],
+	darkMode: ["class"],
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("daisyui"),
+		require("tailwindcss-animate"),
+	],
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -32,19 +37,56 @@ const config: Config = {
 			},
 			colors: {
 				primary: {
-					// Customize it on globals.css :root
-					50: "rgb(var(--tw-color-primary-50) / <alpha-value>)",
-					100: "rgb(var(--tw-color-primary-100) / <alpha-value>)",
-					200: "rgb(var(--tw-color-primary-200) / <alpha-value>)",
-					300: "rgb(var(--tw-color-primary-300) / <alpha-value>)",
-					400: "rgb(var(--tw-color-primary-400) / <alpha-value>)",
-					500: "rgb(var(--tw-color-primary-500) / <alpha-value>)",
-					600: "rgb(var(--tw-color-primary-600) / <alpha-value>)",
-					700: "rgb(var(--tw-color-primary-700) / <alpha-value>)",
-					800: "rgb(var(--tw-color-primary-800) / <alpha-value>)",
-					900: "rgb(var(--tw-color-primary-900) / <alpha-value>)",
+					"50": "rgb(var(--tw-color-primary-50) / <alpha-value>)",
+					"100": "rgb(var(--tw-color-primary-100) / <alpha-value>)",
+					"200": "rgb(var(--tw-color-primary-200) / <alpha-value>)",
+					"300": "rgb(var(--tw-color-primary-300) / <alpha-value>)",
+					"400": "rgb(var(--tw-color-primary-400) / <alpha-value>)",
+					"500": "rgb(var(--tw-color-primary-500) / <alpha-value>)",
+					"600": "rgb(var(--tw-color-primary-600) / <alpha-value>)",
+					"700": "rgb(var(--tw-color-primary-700) / <alpha-value>)",
+					"800": "rgb(var(--tw-color-primary-800) / <alpha-value>)",
+					"900": "rgb(var(--tw-color-primary-900) / <alpha-value>)",
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
 				},
 				dark: "#222222",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
+				},
 			},
 			keyframes: {
 				flicker: {
@@ -70,6 +112,11 @@ const config: Config = {
 			animation: {
 				flicker: "flicker 3s linear infinite",
 				shimmer: "shimmer 1.3s linear infinite",
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
 			},
 		},
 	},
